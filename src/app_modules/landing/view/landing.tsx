1fd488fc36ac6936c12b0668f718d5daccc49f6a
bipproduction <bip.production.js@gmail.com>
2023-08-30 23:06:29 +0800

    baru

---
+++ src/app_modules/landing/view/landing.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { BackgroundImage, Box, Burger, Button, Drawer, Flex, Grid, Group, MediaQuery, Paper, Stack, Text, Title } from "@mantine/core"
+import ViewNavHor from "./nav_hor"
+import { useState } from "react"
+import { useMediaQuery, useShallowEffect } from "@mantine/hooks"
+import ViewNavVer from "./nav_ver"
+
+export default function ViewLanding() {
+    const [open, setOpen] = useState(false)
+    const [client, setClient] = useState(false)
+    const matches = useMediaQuery('(min-width: 56.25em)');
+
+    useShallowEffect(() => {
+        if (window) setClient(true)
+    }, [])
+    return (<>
+        <Stack>
+            <BackgroundImage src="./images/landing.png" h={"100vh"} mih={720}>
+                <Stack>
+                    <Paper pos={"fixed"} w={"100%"} style={{ zIndex: 10 }} bg={"#00000010"}>
+                        <Grid p={"md"}>
+                            <Grid.Col span={"content"}>
+                                <Flex justify={"center"} align={"center"} gap={"md"}>
+                                    {!matches && <Burger onClick={() => setOpen(!open)} opened={open} />}
+                                    <Title>Explore Troica</Title>
+                                </Flex>
+                            </Grid.Col>
+                            <Grid.Col span={"auto"}>
+                                {matches && <ViewNavHor />}
+                            </Grid.Col>
+                        </Grid>
+                    </Paper>
+                    <Stack spacing={"lg"} left={100} pos={"relative"} top={200}>
+                        <Text size={24}>Best Travel Agency</Text>
+                        <Title style={{
+                            overflowWrap: "break-word"
+                        }} size={64}>EXPLORE TROPICA</Title>
+                        <Title>Embark on a Journey to Tropica</Title>
+                        <Box pos={"relative"}>
+                            <Text maw={360}>
+                                Feel free to mix and match these phrases to create a captivating landing page for your website, Explore Tropica. Enjoy your creative process!
+                            </Text>
+                        </Box>
+                        <Group spacing={"md"}>
+                            <Button>DISCOVER</Button>
+                            <Button variant="outline">KNOW MORE</Button>
+                        </Group>
+                    </Stack>
+                </Stack>
+            </BackgroundImage>
+            <Text>Bawahnya</Text>
+        </Stack>
+        <Drawer opened={open} onClose={() => setOpen(false)}>
+            <Stack>
+                <Title>Explore</Title>
+                <ViewNavVer />
+            </Stack>
+        </Drawer>
+    </>)
+}
---
+++ src/app_modules/landing/view/nav_hor.tsx
@@ -0,0 +1,20 @@
+import { Flex, Grid, TextInput, UnstyledButton } from "@mantine/core";
+import { MdSearch } from 'react-icons/md'
+
+export default function ViewNavHor() {
+    return (<>
+        <Grid>
+            <Grid.Col span={"auto"}>
+                <Flex gap={"lg"} align={"center"} p={"md"}>
+                    <UnstyledButton>Home</UnstyledButton>
+                    <UnstyledButton>Destination</UnstyledButton>
+                    <UnstyledButton>Contact Us</UnstyledButton>
+                    <UnstyledButton>Blog</UnstyledButton>
+                </Flex>
+            </Grid.Col>
+            <Grid.Col span={"content"} p={"md"}>
+                <TextInput placeholder="search" icon={<MdSearch />} radius={100} />
+            </Grid.Col>
+        </Grid>
+    </>)
+}
