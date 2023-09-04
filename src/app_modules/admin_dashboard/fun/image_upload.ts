beee32915e969c23395e1fbc76ccdb5061f8d281
bipproduction <bip.production.js@gmail.com>
2023-09-05 01:05:28 +0800

    tambahan

--- src/app_modules/admin_dashboard/fun/image_upload.ts
+++ src/app_modules/admin_dashboard/fun/image_upload.ts
@@ -6,29 +6,33 @@ import { v4 } from 'uuid'
 
 
 export async function funImageUpload({ formData }: { formData: FormData }) {
-    const file: any = formData.get('file')
-    const fileName = file.name;
-    const fileExtension = fileName.split('.').pop();
-    const buffer = Buffer.from(await file.arrayBuffer())
+    try {
+        const file: any = formData.get('file')
+        const fileName = file.name;
+        const fileExtension = fileName.split('.').pop();
+        const buffer = Buffer.from(await file.arrayBuffer())
 
-    const dataImg = await prisma.imageGalery.create({
-        data: {
-            name: fileName,
-            ext: fileExtension
+        const dataImg = await prisma.imageGalery.create({
+            data: {
+                name: fileName,
+                ext: fileExtension
+            }
+        })
+
+        const img = await sharp(buffer).resize(200).toBuffer()
+
+        fs.writeFileSync(`./public/images/${dataImg.id}.${fileExtension}`, img)
+        return {
+            success: true,
+            message: "success",
+            data: {
+                id: dataImg.id,
+                name: dataImg.name,
+                ext: dataImg.ext
+            }
         }
-    })
-
-    const img = await sharp(buffer).resize(200).toBuffer()
-
-    fs.writeFileSync(`./public/images/${dataImg.id}.${fileExtension}`, img)
-    return {
-        success: true,
-        message: "success",
-        data: {
-            id: dataImg.id,
-            name: dataImg.name,
-            ext: dataImg.ext
-        }
+    } catch (error) {
+        return null
     }
 
 }
