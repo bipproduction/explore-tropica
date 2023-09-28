46efaafe3cc40c33e41fc38a65064ff3dfe72558
bipproduction <bip.production.js@gmail.com>
2023-09-28 14:57:21 +0800

    title: auto des: auto note:auto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
 
-        {/* <link href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
-        {/* <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@100&display=swap" rel="stylesheet" /> */}
-
       </head>
       <body suppressHydrationWarning={true}>
         <MantineProvider theme={theme}>{children}</MantineProvider>
