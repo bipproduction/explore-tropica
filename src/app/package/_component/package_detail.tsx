07d01a76d99e8130b4719be354419c97cff2987a
bipproduction <bip.production.js@gmail.com>
2023-09-30 18:56:29 +0800

    title: auto des: auto note:auto

--- src/app/package/_component/package_detail.tsx
+++ src/app/package/_component/package_detail.tsx
@@ -3,11 +3,19 @@ import { ActionIcon, Flex, Group, Image, Stack, Text, Title } from "@mantine/cor
 import { MdArrowBackIos } from "react-icons/md";
 import parse from 'html-react-parser'
 import { useRouter } from "next/navigation";
+import { useShallowEffect } from "@mantine/hooks";
+import { useState } from "react";
 
 export default function ComPackageDetail({ data }: { data: any }) {
     const router = useRouter()
+    const content = (data.des as string).replace('+6287765344998', '').replace('/', '').replace('reservationexploretropica@gmail.com', 'suryabisnisakun@gmail.com')
+    const client = useState(false)
+    useShallowEffect(() => {
+        if(window) client[1](true)
+    }, [])
+if(!client[0]) return <></>
     return <>
-        <Stack >
+        <Stack suppressHydrationWarning={true}>
             <Flex p={"md"}>
                 <ActionIcon onClick={router.back}>
                     <MdArrowBackIos size={32} />
@@ -17,7 +25,7 @@ export default function ComPackageDetail({ data }: { data: any }) {
                 <Stack maw={720} p={"md"}>
                     <Image src={(data.img as string).replace('/img/', '/images/')} alt="" height={500} />
                     <Title>{data.title}</Title>
-                    {parse((data.des as string).replace('+6287765344998', '').replace('/', '').replace('reservationexploretropica@gmail.com', 'suryabisnisakun@gmail.com'))}
+                    {parse(content)}
                 </Stack>
             </Group>
         </Stack>
